75acc02967827e33a6ae750f353ae074523771b0
Maya Adar <mayabee2@gmail.com>
2024-03-06 20:51:54 +0200

    apply methods on forms

--- pages/home_page/home_page.py
+++ pages/home_page/home_page.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask import render_template, redirect, url_for
 from flask import Blueprint
 
@@ -12,12 +13,17 @@ home_page = Blueprint(
 
 
 # Routes
-@home_page.route('/')
+@home_page.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        print(email, password)
     return render_template('home_page.html')
 
-@home_page.route('/homepage')
-@home_page.route('/home')
+@home_page.route('/homepage', methods=['GET', 'POST'])
+@home_page.route('/home', methods=['GET', 'POST'])
 def redirect_homepage():
     return redirect(url_for('home_page.index'))
 
