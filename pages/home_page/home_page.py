84228e113620880620ce2f9fdb5b6944de8c40c9
Maya Adar <mayabee2@gmail.com>
2024-03-20 18:21:09 +0200

    Add git ignore

--- pages/home_page/home_page.py
+++ pages/home_page/home_page.py
@@ -1,4 +1,6 @@
 from flask import *
+from app import db
+from mongodb import *
 
 # about blueprint definition
 home_page = Blueprint(
@@ -13,6 +15,8 @@ home_page = Blueprint(
 # Routes
 @home_page.route('/', methods=['GET', 'POST'])
 def index():
+    movie_names = MongoDB.get_movies(db)
+    # cities = [city['city_name'] for city in db.cities.find({}, {'_id': 0, 'city': 1})] # get cities for dropdown list
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
@@ -21,10 +25,9 @@ def index():
 
     print("Email stored in session:", session.get('email'))  # Debugging statement
 
-    return render_template('home_page.html')
+    return render_template('home_page.html', movie_names=movie_names, cities=cities)
 
 @home_page.route('/homepage', methods=['GET', 'POST'])
 @home_page.route('/home', methods=['GET', 'POST'])
 def redirect_homepage():
     return redirect(url_for('home_page.index'))
-
